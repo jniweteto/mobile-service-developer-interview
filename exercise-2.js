c199b13492fb1e3df7b525fcdf28507bb97b2af9
jniweteto <51609841+jniweteto@users.noreply.github.com>
2020-02-08 12:01:46 -0500

    Update exercise-2.js

--- exercise-2.js
+++ exercise-2.js
@@ -9,11 +9,15 @@ var admin_alert = require('./lib/admin-alert'); //global admin alerter
 var get_menu_option = require('./lib/get-menu-option'); 
 var populate_menu = require('./lib/populate-menu');
 
+
 // initialize variables and constants
-const lang = 'swahili';
+const lang = 'kinyarwanda';
 const max_digits_for_account_number = 8;
 const max_digits_for_input = 2;
 const timeout_length = 60;
+
+//maximu number of chacters for each description
+const max_issue_description =180;
 
 // main function; prompts client to enter their account number
 global.main = function(){
@@ -97,6 +101,44 @@ addInputHandler('menu_select', function(input){
 */
 addInputHandler('issue_menu_select', function(input){
     // ADD YOUR CODE HERE
+    
+    // since the user would need to enter issue description, there is no need to replace non-digital text by empty space
+    
+    //selection issue form the issue menu
+    var selection = get_menu_option(input, 'issue_menu');
+
+    //Wrong selection of the menu/ when menue not listed
+    if(selection === null || selection === undefined){
+        sayText(msgs('invalid_input', {}, lang));
+        promptDigits('issue_menu_select', {'submitOnHash' : false, 'maxDigits' : max_digits_for_input,'timeout' : timeout_length});
+        return null;
+    }
+
+    //When user select 2, I assume that there other people in charge to send alerts to when the user select another number than 2
+    //depending on number of issues defined.
+    else if(selection === 2){
+     
+        //prompt the to enter a brief description of the issue
+        sayText(msgs('msg_prompt', lang));
+
+        //saving the isse description so that we would send it to Arsene who is in charge of fraud and harasement
+        // From my experience, it could take 2.5 min on average to type a 180-characters message using a smartphone. I think that time 
+        //becomes longer when you use a feature phone which I think most farmers have. So the timeout here should be longer. 
+        //I assume afarmer who is not used to typing message would take 5min(300 seconds) on average
+        let issueDescription = promptDigits('send', {'submitOnHash' : false,'maxIssueText' : max_issue_description, 'timeout' : 300});
+
+        //alerting arsene about the issue described by the user and send him user's account
+        admin_alert('Fraud/harasement issue from user ' +'Account number' +'\n'+ issueDescription,"Arsene")
+
+        sayText(msgs('msg_received',lang));
+        return null;
+    }
+
+    else{
+        sayText(msgs('invalid_input', lang));  
+        promptDigits('issue_menu_select', {'submitOnHash' : false, 'maxDigits' : max_digits_for_input,'timeout' : timeout_length});
+        return null;  
+    }
 });
 
 // let the client know that their message has been received
